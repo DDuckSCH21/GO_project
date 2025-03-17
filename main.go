71b6bdfcf49546ee8c7168e699a33a80a992882f
DDuckSCH21 <AlexanderTrefiloff@yandex.ru>
2025-03-17 12:43:44 +0500

    Add switch for Methods

--- main.go
+++ main.go
@@ -1,17 +1,50 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type users struct {
+	id       int
+	name     string
+	lastName string
+	age      int
+}
+
+var globalDB map[int]users
+
+func pathHandler(r *http.Request) {
+	parts := strings.Split(r.URL.Path, "/")
+	fmt.Println(parts)
+}
+
+func getUsers(w *http.ResponseWriter) {
+	// todo вернуть всех users из globalDB
+}
 
 func usersHandler(w http.ResponseWriter, r *http.Request) {
+
+	pathHandler(r)
+
+	switch r.Method {
+	case http.MethodGet:
+		getUsers(&w)
+	case http.MethodPut:
+	case http.MethodPost:
+	case http.MethodDelete:
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
 
 }
 
 func main() {
-	http.HandleFunc(":8080/users", usersHandler)
-
-	http.HandleFunc(":8080/users/:id", usersHandler)
+	http.HandleFunc("/users", usersHandler)
 
 	http.ListenAndServe(":8080", nil)
+
 }
 
 ///users/:id
