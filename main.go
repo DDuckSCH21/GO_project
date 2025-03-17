14a6fe551103918e43f3e7548a379904abb89e28
DDuckSCH21 <AlexanderTrefiloff@yandex.ru>
2025-03-17 13:05:11 +0500

    Add emptyFunc

--- main.go
+++ main.go
@@ -15,25 +15,43 @@ type users struct {
 
 var globalDB map[int]users
 
-func pathHandler(r *http.Request) {
+func pathHandler(r *http.Request) []string {
 	parts := strings.Split(r.URL.Path, "/")
 	fmt.Println(parts)
+	return parts
 }
 
-func getUsers(w *http.ResponseWriter) {
-	// todo вернуть всех users из globalDB
+func getUsers(w *http.ResponseWriter, parts []string) { //Возвращает всех users
+	//TODO вернуть всех users из globalDB
+	//ИЛИ конкретного user по id
+}
+
+func putUser(w *http.ResponseWriter, parts []string) { //Обновляет данные по id
+	//TODO найти в globalDB и обновить те данные, которые пришли
+}
+
+func postUser(w *http.ResponseWriter, parts []string) { //Создает нового user
+	//Добавить запись в globalDB, вернуть новый id
+	//Как-то надо считать порядок id
+}
+
+func deleteUser(w *http.ResponseWriter, parts []string) { //Удаляет user по id
+	//берем part[2] и по нему удаляем из globalDB
 }
 
 func usersHandler(w http.ResponseWriter, r *http.Request) {
 
-	pathHandler(r)
+	parts := pathHandler(r)
 
 	switch r.Method {
-	case http.MethodGet:
-		getUsers(&w)
-	case http.MethodPut:
-	case http.MethodPost:
-	case http.MethodDelete:
+	case http.MethodGet: //GET /users OR GET /users/:id
+		getUsers(&w, parts)
+	case http.MethodPut: //PUT /users/:id
+		putUser(&w, parts)
+	case http.MethodPost: //POST /users
+		postUser(&w, parts)
+	case http.MethodDelete: //DELETE /users/:id
+		deleteUser(&w, parts)
 	default:
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
