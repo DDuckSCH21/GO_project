b84fb98cbcf28ea401d20a1cda11a07a277d5061
DDuckSCH21 <AlexanderTrefiloff@yandex.ru>
2025-04-25 11:19:14 +0500

    add sql.init; чет не создает новые таблицы

--- DB/init.sql
+++ DB/init.sql
@@ -1,3 +1,24 @@
--- Создание базы, если ее нет
-
 -- docker exec -it GO_project-RestAPI psql -U myuser -d GO_project -c "SHOW database"
+
+-- Создание базы, если ее нет
+CREATE DATABASE my_restapi;
+
+-- Подключение к новой БД
+\c my_restapi
+
+-- Создание таблиц
+CREATE TABLE public.users (
+	id int NOT NULL,
+	"name" varchar(128) NOT NULL,
+	age smallint NOT NULL,
+	is_student bool NOT NULL,
+	CONSTRAINT users_pk PRIMARY KEY (id)
+);
+
+
+-- Начальные данные
+INSERT INTO users ("name", age, is_student) 
+VALUES 
+    ('admin', 14, true),
+    ('user1', 21, false)
+ON CONFLICT (username) DO NOTHING;
