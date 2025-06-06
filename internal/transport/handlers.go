7ec78e602ab3a5df75d955ce4b2622ce5c584812
DDuckSCH21 <AlexanderTrefiloff@yandex.ru>
2025-06-06 19:33:36 +0500

    clear final version

--- internal/transport/handlers.go
+++ internal/transport/handlers.go
@@ -1,16 +1,12 @@
 package transport
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
-	"go_project/internal/global"
 	"go_project/internal/models"
 	"net/http"
-	"strconv"
-	"strings"
 )
 
 //FOR DATABASE
@@ -142,144 +138,11 @@ func sendStatus(status int, w http.ResponseWriter) {
 	}
 }
 
-func pathHandler(r *http.Request, w http.ResponseWriter) int {
-	parts := strings.Split(r.URL.Path, "/")
-	id, err := strconv.Atoi(parts[len(parts)-1])
-	if err != nil {
-		http.Error(w, "Error: Parsing path. strconv.Atoi", http.StatusBadRequest) //TODO Обработать ошибку //Колхозненько
-		return -1
-	}
-	return id
-}
-
-func getIdUsers(w http.ResponseWriter, id int) { //Возвращает конкретного user по id
-	user, ok := global.DB.Get(id) //TODO: Заменить на БД
-	if ok {
-		sendStatus(http.StatusOK, w) // 200 - по дефолту отправляется, не надо еще раз это делать
-		fmt.Fprintf(w, "User ID = %d: %v\n", id, user.Data)
-	} else {
-		http.Error(w, "User not found", http.StatusNotFound)
-	}
-}
-
-func getAllUsers(w http.ResponseWriter) { //Возвращает всех users
-
-	if !global.DB.IsEmpty() {
-		buf := global.DB.GetAll()
-		sendStatus(http.StatusOK, w)
-		fmt.Fprintln(w, &buf)
-	} else {
-		fmt.Fprintln(w, "No Data")
-	}
-}
-
-func putIdUser(w http.ResponseWriter, r *http.Request, id int) { //Обновляет данные по id
-	defer r.Body.Close()
-	var newUser models.User
-
-	_, ok := global.DB.Get(id)
-	if ok {
-		err := json.NewDecoder(r.Body).Decode(&newUser.Data)
-		if err != nil {
-			http.Error(w, "Error: Decode JSON", http.StatusBadRequest)
-			return
-		}
-		global.DB.Set(id, newUser) //TODO Проверить. Хрень будто какая-то нерабочая
-		sendStatus(http.StatusOK, w)
-	} else {
-		http.Error(w, "Error: id not found", http.StatusNotFound)
-	}
-}
-
-func postUser(w http.ResponseWriter, r *http.Request) { //Добавить новую запись в global.DB, возвращает новый id
-	defer r.Body.Close()
-
-	var user models.User
-
-	err := json.NewDecoder(r.Body).Decode(&user.Data) //пока так
-	if err != nil {
-		if err.Error() == "EOF" {
-			http.Error(w, "Error: Empty Request", http.StatusBadRequest)
-		} else {
-			http.Error(w, "Error: Decode JSON", http.StatusBadRequest)
-		}
-		return
-	}
-	user.Id = global.DB.GetNewKey()
-	global.DB.Set(user.Id, user)
-	sendStatus(http.StatusCreated, w)
-	fmt.Fprintf(w, "Add new User id=[%d]\n", user.Id)
-}
-
-func deleteIdUser(w http.ResponseWriter, id int) { //Удаляет user по id
-	ok := global.DB.Del(id)
-	if ok {
-		sendStatus(http.StatusOK, w)
-	} else {
-		fmt.Fprintf(w, "User ID = %d not found\n", id)
-	}
-}
-
-func UsersIdHandler(w http.ResponseWriter, r *http.Request) {
-	idUser := pathHandler(r, w)
-
-	switch r.Method {
-	case http.MethodGet: //GET /users/:id
-		getIdUsers(w, idUser)
-	case http.MethodPut: //PUT /users/:id
-		putIdUser(w, r, idUser)
-	case http.MethodDelete: //DELETE /users/:id
-		deleteIdUser(w, idUser)
-	default:
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	}
-}
-
-func UsersHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet: //GET /users
-		getAllUsers(w)
-	case http.MethodPost: //POST /users
-		postUser(w, r)
-	default:
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	}
-}
-
 func MasterHandler(r *chi.Mux, db *pgxpool.Pool) {
-
-	// fmt.Println("INSERT")
-	rows, err := db.Query(context.Background(), "INSERT INTO public.users (name,age,is_student) VALUES ('SanPusan',21,true);") //TODO Заготовка для postUser
-
-	if err != nil {
-		panic(err)
-	}
-	// fmt.Printf("RESULT INSERT%s\n", rows)
-	defer rows.Close()
-
-	// fmt.Println("SELECT")
-	rows_2, err := db.Query(context.Background(), "SELECT id, name FROM public.users") //TODO Заготовка для getAllUsers
-	if err != nil {
-		panic(err)
-	}
-
-	for rows_2.Next() {
-		var id int
-		var name string
-		err = rows_2.Scan(&id, &name)
-		if err != nil {
-			panic(err)
-		}
-		// fmt.Printf("ID: %d, Name: %s\n", id, name)
-	}
-	defer rows_2.Close()
 
 	r.Get("/users", getAllUsersDB(db))
 	r.Get("/users/{id}", getIdUsersDB(db))
 	r.Put("/users/{id}", putIdUserDB(db))
 	r.Delete("/users/{id}", deleteIdUserDB(db))
 	r.Post("/users", postUserDB(db))
-
-	// r.HandleFunc("/users", UsersHandler)
-	// r.HandleFunc("/users/{id}", UsersIdHandler)
 }
